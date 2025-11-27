0b7f057fbed2e909777424ea0f1cc95adf0d4539
Mani Teja Chalavadi <manitejachalavadi765@gmail.com>
2025-11-27 10:57:51 +0530

    Adding Project to GitHub from IntelliJ

---
+++ src/main/java/com/FT/finance_tracker_API/FinanceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.FT.finance_tracker_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApiApplication.class, args);
+	}
+
+}
